08fdc4a7209c8e9368240e4cf0cfe308e38132ae
hoon <nasi3611@naver.com>
2023-05-13 00:18:18 +0900

    파이썬에서 GC 예제
    파이썬에서 GC 예제

--- Leak.py
+++ Leak.py
@@ -10,8 +10,7 @@ def run():
     objs = []
     for i in range(10):
         objs.append(MyObject())
-        time.sleep(0.1)  # Memory leak를 일으키기 위해 잠시 대기
-
-    # 함수 종료 후 objs 리스트는 더 이상 사용되지 않으므로 제거되어야 함 --> Memory leak 발생
+        time.sleep(0.1)  # Memory leak를 일으키기 위해 잠시 대기 
+    # 함수 종료 후 objs 리스트는 더 이상 사용되지 않으므로 제거되어야 함 ---> Memory leak 발생
     # objs 리스트와 그 안에 MyObject 인스턴스들도 Memory에서 삭제되지 않음!
     # 해결 법: objs 리스트를 전역 범위로 옮기거나 함수 내에서 명시적으로 삭제해야 함! -> solveLeak.py에서 구현
